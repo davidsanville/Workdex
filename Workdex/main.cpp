76bf47ec7f68e81fbe7d5bbfad7d0e74f4ddec60
aurelius <davids94@comcast.net>
2018-01-17 00:54:14 -0800

    Initial Commit

---
+++ Workdex/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Workdex
+//
+//  Created by David Sanville on 1/17/18.
+//  Copyright © 2018 David Sanville. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
