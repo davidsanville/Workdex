1b924bc0c25a5137a4b96417e30109c1344b202c
aurelius <davids94@comcast.net>
2018-01-25 23:06:42 -0800

    Added UI struct

--- Workdex/main.cpp
+++ Workdex/main.cpp
@@ -7,9 +7,45 @@
 //
 
 #include <iostream>
+#include <string>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+const int dummy = 8;
+
+struct UI {
+    string items[dummy];
+    string stories[dummy]; //replace with some multi-node structure, for epics
+    stack<string> orderedTasks;
+    
+    void initializeDummy() {
+        for (int i =0; i<dummy; i++) items[i] = "item";
+    }
+    
+    void display() {
+        for (int i=0; i<dummy; i++)  cout << items[i];
+    }
+    
+    void fillItems() {
+        cout << "User fill here";
+        //use panda for CSV upload integration?
+    }
+    
+};
+
+
+//at the end of each task 1 of 4 things can happen
+enum result {complete, stuck, delayed, nested};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    UI foo;
+    foo.initializeDummy();
+    foo.display();
+    //foo.fillItems();
+    
     return 0;
 }
